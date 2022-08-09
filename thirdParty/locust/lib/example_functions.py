e73f2d71f62113f970196502ff03bd30694e0202
Stephen Atwell <stephen.atwell@armory.io>
2022-08-09 16:42:08 -0700

    added a demo that leverages locust for performance testing

---
+++ thirdParty/locust/lib/example_functions.py
@@ -0,0 +1,17 @@
+
+
+# -*- coding: utf-8 -*-
+
+import random
+
+
+def choose_random_page():
+    pages = [
+        #'/policies/privacy/',
+        #'/contact/',
+        #'/about/',
+        #'/search/howsearchworks/crawling-indexing/',
+        '/'
+    ]
+
+    return random.choice(pages)
---
+++ thirdParty/locust/readme.txt
@@ -0,0 +1,1 @@
+Deployment based off https://medium.com/teamsnap-engineering/load-testing-a-service-with-20-000-requests-per-second-with-locust-helm-and-kustomize-ea9bea02ae28
