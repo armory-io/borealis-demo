32a557cd5c15ad2da15231f76e9ba8c86a8ab6df
parth <parth.srivastav@armory.io>
2022-10-20 10:36:20 -0700

    Adding cmdhook manifest and deploy command during setup

--- configuration/argo-rollouts.sh
+++ configuration/argo-rollouts.sh
@@ -19,9 +19,13 @@ esac
 
 curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-${OS}-amd64
 chmod +x ./kubectl-argo-rollouts-${OS}-amd64
-#BASEDIR=$(dirname $0)
+BASEDIR=$(dirname $0)
 #export PATH=$PATH:$BASEDIR/kubectl-argo-rollouts-${OS}-amd64
 echo "running command with sudo, enter your password"
 sudo mv ./kubectl-argo-rollouts-${OS}-amd64 /usr/local/bin/kubectl-argo-rollouts
 
+echo "Initialising Argo Dashboard. https://localhost:3001"
 kubectl argo rollouts dashboard -n borealis-argo &
+
+echo "Initialising Command micro service"
+kubectl apply -f "$BASEDIR/../thirdParty/cmdhook.yml" -n borealis-demo-agent-prod
