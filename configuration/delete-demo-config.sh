00368292c75cdac6066a571573d5de9f37e489d6
Stephen Atwell <78044713+stephenatwell@users.noreply.github.com>
2022-11-17 16:32:43 -0800

    Update delete-demo-config.sh

--- configuration/delete-demo-config.sh
+++ configuration/delete-demo-config.sh
@@ -1,5 +1,11 @@
 helm uninstall armory-rna-prod \
     -n borealis-demo-agent-prod
+helm uninstall armory-rna-prod-eu \
+    -n borealis-demo-agent-prod-eu
+helm uninstall armory-rna-staging \
+    -n borealis-demo-agent-staging
+helm uninstall armory-rna-dev \
+    -n borealis-demo-agent-dev
 helm uninstall prometheus -n=borealis-demo-infra
 #do not delete demo agent prod, we want the secret in it kept around!!!
 #kubectl delete namespace borealis-demo-agent-prod
