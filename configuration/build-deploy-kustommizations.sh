e3426543ec377a946dcfa186abae16433fe83e17
Stephen Atwell <stephen.atwell@armory.io>
2023-09-26 09:57:55 -0700

    got lambda deployment generating of single config. renamed deploy targes in k8s to be more intuitive, and update app name to match in reset.sh

--- configuration/build-deploy-kustommizations.sh
+++ configuration/build-deploy-kustommizations.sh
@@ -10,15 +10,36 @@ build()
   echo "yq eval 'del(.metadata)' ../$DESTINATION -i"
   yq eval 'del(.metadata)' ../$DESTINATION -i
   echo "reordering output...."
-  yq eval '{"version": .version, "kind": .kind,"application": .application, "targets":.targets, "manifests":.manifests,"strategies": .strategies,"analysis": .analysis,"trafficManagement": .trafficManagement, "webhooks": .webhooks}'  ../$DESTINATION -i
+  yq eval '{"version": .version, "kind": .kind,"application": .application, "targets":.targets, "manifests":.manifests,"artifacts":.artifacts,"providerOptions":.providerOptions,"strategies": .strategies,"analysis": .analysis,"trafficManagement": .trafficManagement, "webhooks": .webhooks, "deploymentConfig": .deploymentConfig}'  ../$DESTINATION -i
   yq eval '(.targets | key) line_comment="#This section defines the targets to which you are deploying, and their constraints."' ../$DESTINATION -i
   yq eval '(.manifests | key) line_comment="#This section defines the Manifests you are deploying, by default they reach all targets, but you can specify certain targets if needed."' ../$DESTINATION -i
   yq eval '(.strategies | key) line_comment="#This section defines the strategies environments can use to deploy."' ../$DESTINATION -i
   yq eval '(.analysis | key) line_comment="#This section defines queries against your monitoring system that can be used for automated canary analysis."' ../$DESTINATION -i
+  yq eval '(.providerOptions | key) line_comment="#This section defines options specific to the cloud provider to which we are deploying."' ../$DESTINATION -i
   yq eval '(.trafficManagement | key) line_comment="#Traffic management configuration"' ../$DESTINATION -i
   yq eval '(.webhooks | key) line_comment="#Webhooks can be used to run external automation."' ../$DESTINATION -i
+  yq eval '.analysis.queries[].lowerLimit=0.0' ../$DESTINATION -i
+  if [ $3 == "lambda" ]
+  then
+    yq eval 'del(.manifests)' ../$DESTINATION -i
+    yq eval '.kind="lambda"' ../$DESTINATION -i
+    yq eval '(.artifacts | key) line_comment="#This section defines the artifacts you are deploying, by default they reach all targets, but you can specify certain targets if needed."' ../$DESTINATION -i
+    #temporary, remove features we do not yet support.
+    yq eval 'del(.strategies.myBlueGreen)' ../$DESTINATION -i
+    yq eval 'del(.strategies.mycanary)' ../$DESTINATION -i
+    yq eval 'del(.strategies.rolling)' ../$DESTINATION -i
+    yq eval 'del(.trafficManagement)' ../$DESTINATION -i
+    #end temporary
+  fi
+  if [ $3 == "k8s" ]
+  then
+    yq eval '(.manifests | key) line_comment="#This section defines the Manifests you are deploying, by default they reach all targets, but you can specify certain targets if needed."' ../$DESTINATION -i
+    yq eval 'del(.artifacts)' ../$DESTINATION -i
+    yq eval 'del(.providerOptions)' ../$DESTINATION -i
+  fi
 }
 
 brew install yq
-build deploy-vanilla deploy.yml
-build deploy-argo deploy-w-argo.yml
+build deploy-vanilla deploy.yml k8s
+build deploy-lambda lambda-deploy.yml lambda
+build deploy-argo deploy-w-argo.yml k8s
--- reset.sh
+++ reset.sh
@@ -10,7 +10,6 @@ cd reset
 #kubectl -n=borealis-prod delete rs --all
 #kubectl -n=borealis-prod2 delete rs --all
 #sleep 60
-armory deploy start -f deploy3.yml
-sleep 60
-cd ..
+armory deploy start -f deploy3.yml -w
+
 
